21db7c47e9583f8326dc20e816ce968b6e5c3259
vennilamurugavel <vennilageo@gmail.com>
2023-10-28 20:59:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+let timer=10;
+let time=document.createElement("p");
+time.setAttribute("id","display");
+time.style.width="400px";
+// time.style.height="100px";
+time.style.backgroundColor="pink";
+time.style.textAlign="center";
+time.style.padding="30px";
+time.style.marginLeft="300px";
+time.style.fontSize="30px";
+
+document.body.append(time);
+setTimeout(()=>{
+    time.innerHTML=timer--;
+
+    setTimeout(()=>{
+        time.innerHTML=timer--;
+        time.style.backgroundColor="red";
+  
+    setTimeout(()=>{
+        time.innerHTML=timer--;
+        time.style.backgroundColor="yellow";
+
+
+    setTimeout(()=>{
+        time.innerHTML=timer--;
+        time.style.backgroundColor="green";
+
+
+    setTimeout(()=>{
+        time.innerHTML=timer--;
+        time.style.backgroundColor="blue";
+
+  
+    setTimeout(()=>{
+        time.innerHTML=timer--;
+        time.style.backgroundColor="aqua";
+
+   
+    setTimeout(()=>{
+        time.innerHTML=timer--;
+        time.style.backgroundColor="blueviolet";
+
+  
+    setTimeout(()=>{
+        time.innerHTML=timer--;
+        time.style.backgroundColor="brown";
+
+
+    setTimeout(()=>{
+        time.innerHTML=timer--;
+        time.style.backgroundColor="chocolate";
+
+   
+    setTimeout(()=>{
+        time.innerHTML=timer--;
+        time.style.backgroundColor="crimson";
+
+ 
+   
+    setTimeout(()=>{
+        time.innerHTML="Happy Independance Day";
+       time.style. background="linear-gradient(orange,white,green)";
+
+       
+
+   
+},1000) 
+},1000) 
+},1000) 
+},1000) 
+},1000)
+},1000)   
+},1000)  
+},1000) 
+},1000)  
+},1000)
+},1000); 
+                  
+
